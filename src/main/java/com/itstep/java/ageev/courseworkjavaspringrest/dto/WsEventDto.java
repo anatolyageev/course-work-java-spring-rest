86626ea7f8ba764393c75ee06862a6d5e1be36cf
Anatolii Ageev <anatol1812@gmail.com>
2021-03-15 22:00:54 +0200

    feature complited

---
+++ src/main/java/com/itstep/java/ageev/courseworkjavaspringrest/dto/WsEventDto.java
@@ -0,0 +1,18 @@
+package com.itstep.java.ageev.courseworkjavaspringrest.dto;
+
+import com.fasterxml.jackson.annotation.JsonRawValue;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.itstep.java.ageev.courseworkjavaspringrest.domain.Views;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@JsonView(Views.Id.class)
+public class WsEventDto {
+    private ObjectType objectType;
+    private EventType eventType;
+    @JsonRawValue
+    private String body;
+
+}
--- src/main/resources/static/js/components/messages/MessageForm.vue
+++ src/main/resources/static/js/components/messages/MessageForm.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-    import { sendMessage } from "../../util/ws";
+    import messagesApi from 'api/messages'
 
 
 
@@ -31,27 +31,32 @@
         },
         methods: {
             save() {
+                const message = {
+                    id: this.id,
+                    text: this.text
+                }
+                if (this.id) {
+                    messagesApi.update( message)
+                        .then(result => result.json().then(data => {
+                            const index = this.messages.findIndex(item=>item.id === data.id)
+                            this.messages.splice(index, 1, data);
 
-                sendMessage({id: this.id, text: this.text});
+                        }))
+
+
+                } else {
+                    messagesApi.add( message).then(result =>
+                        result.json().then(data => {
+                            const index = this.messages.findIndex(item=>item.id === data.id)
+                            if(index>-1){
+                                this.messages.splice(index,1,data)
+                            }else {
+                                this.messages.push(data)
+                            }
+                        }))
+                }
                 this.text = '';
                 this.id = '';
-                // var message = {text: this.text};
-                // if (this.id) {
-                //     this.$resource('/message{/id}').update({id: this.id}, message)
-                //         .then(result => result.json().then(data => {
-                //             const index = getIndex(this.messages, data.id)
-                //             this.messages.splice(index, 1, data);
-                //
-                //         }))
-                //
-                //
-                // } else {
-                //     this.$resource('/message{/id}').save({}, message).then(result =>
-                //         result.json().then(data => {
-                //             this.messages.push(data);
-                //             this.text = '';
-                //         }))
-                // }
             }
         }
     }
