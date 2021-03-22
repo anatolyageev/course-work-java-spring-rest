ea943622e1e5bc91cea3773499443e8d4f571b25
Anatolii Ageev <anatol1812@gmail.com>
2021-03-23 00:11:57 +0200

    video 15 1:47

--- src/main/java/com/itstep/java/ageev/courseworkjavaspringrest/service/CommentService.java
+++ src/main/java/com/itstep/java/ageev/courseworkjavaspringrest/service/CommentService.java
@@ -1,18 +1,26 @@
 package com.itstep.java.ageev.courseworkjavaspringrest.service;
 
 import com.itstep.java.ageev.courseworkjavaspringrest.domain.Comment;
+import com.itstep.java.ageev.courseworkjavaspringrest.domain.Message;
 import com.itstep.java.ageev.courseworkjavaspringrest.domain.User;
+import com.itstep.java.ageev.courseworkjavaspringrest.domain.Views;
+import com.itstep.java.ageev.courseworkjavaspringrest.dto.EventType;
+import com.itstep.java.ageev.courseworkjavaspringrest.dto.ObjectType;
 import com.itstep.java.ageev.courseworkjavaspringrest.repository.CommentRepository;
+import com.itstep.java.ageev.courseworkjavaspringrest.util.WsSender;
+import java.util.function.BiConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final BiConsumer<EventType, Comment> wsSender;
 
     @Autowired
-    public CommentService(CommentRepository commentRepository) {
+    public CommentService(CommentRepository commentRepository, WsSender wsSender) {
         this.commentRepository = commentRepository;
+        this.wsSender = wsSender.getSender(ObjectType.COMMENT, Views.FullComment.class);
     }
 
     public Comment create(Comment comment, User user){
